d1b5b3328c1b7fdfdbafe1067010c98b7f59a1b2
Abdullaev-kosimzho-002 <dzukosta348@gmail.com>
2021-12-21 16:27:16 +0300

    Third homework

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ternary_operator_home_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               //Ternary 1.
+          
+           
+           // int number_1 =Convert.ToInt32(Console.ReadLine());
+           // Console.WriteLine(number_1);
+           // int result = (number_1 > 0) ? (number_1 += 1) : (number_1);
+           // Console.WriteLine(result);
+           //
+
+           //Ternary 2.
+
+           // int number_2 = Convert.ToInt32(Console.ReadLine());
+           // Console.WriteLine(number_2);
+           // int secondNumber = (number_2 > 0) ? (number_2 += 1) : (number_2 -= 2);
+           // Console.WriteLine(secondNumber);
+           //
+
+          // Ternary 3.
+
+         //  int number1 = Convert.ToInt32(Console.ReadLine());
+         //  Console.WriteLine(number1);
+         //  int firstStep = (number1 > 0) ? (number1 += 1) : (number1 -= 2);
+         // Console.WriteLine(firstStep);
+         //
+         
+        }
+    }
+}
